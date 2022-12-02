792b67c1554cdb5b8683a7215d867fbae2c50d09
CLAUDIO-FLORES1 <CLAUDIOFLRS1@GMAIL.COM>
2022-12-01 22:09:52 -0500

    did some code clean up

--- app/routes/exhibit_routes.js
+++ app/routes/exhibit_routes.js
@@ -1,32 +1,16 @@
 const express = require('express')
 const passport = require('passport')
-// const mongoose = require('mongoose')
 const Exhibit = require('../models/exhibit')
-// const Review = require('../models/review')
 
 const customErrors = require('../../lib/custom_errors')
 
 const handle404 = customErrors.handle404
-// const requireOwnership = customErrors.requireOwnership
 
 const removeBlanks = require('../../lib/remove_blank_fields')
 const requireToken = passport.authenticate('bearer', { session: false })
 
 
 const router = express.Router()
-
-// INDEX
-// GET
-// router.get('/exhibits', (req, res, next) => {
-// 	console.log("WORKING")
-// 	console.log(mongoose.connection.db.listCollections().toArray((err, collection) => console.log(collection)))
-// 	Exhibit.find()
-// 		.then((exhibits) => {
-// 			console.log("LOG", exhibits)
-// 			res.status(200).json({ exhibits: exhibits })
-// 		})
-// 		.catch(next)
-// })
 
 // INDEX
 // GET
@@ -87,15 +71,7 @@ router.delete('/exhibits/:id', requireToken, (req, res, next) => {
 	Exhibit.findById(req.params.id)
 		.then(handle404)
 		.then((exhibit) => {
-				// throw an error if current user doesn't own `example`
-				// requireOwnership(req, exhibit)
-				// delete the example ONLY IF the above didn't throw
 				exhibit.deleteOne()
-			// requireOwnership(req, exhibit)
-			//Also delete reviews of exhibit
-			// Review.deleteMany({ exhibit: exhibit.id })
-			// 	.catch(next)
-			// Exhibit.deleteOne()
 		})
 		.then(() => res.sendStatus(204))
 		.catch(next)
--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,17 +1,13 @@
-
-   
 const express = require('express')
-// jsonwebtoken docs: https://github.com/auth0/node-jsonwebtoken
+
 const crypto = require('crypto')
-// Passport docs: http://www.passportjs.org/docs/
+
 const passport = require('passport')
-// bcrypt docs: https://github.com/kelektiv/node.bcrypt.js
+
 const bcrypt = require('bcrypt')
 
-// see above for explanation of "salting", 10 rounds is recommended
 const bcryptSaltRounds = 10
 
-// pull in error types and the logic to handle them and set status codes
 const errors = require('../../lib/custom_errors')
 
 const BadParamsError = errors.BadParamsError
@@ -19,9 +15,6 @@ const BadCredentialsError = errors.BadCredentialsError
 
 const User = require('../models/user')
 
-// passing this as a second argument to `router.<verb>` will make it
-// so that a token MUST be passed for that route to be available
-// it will also set `res.user`
 const requireToken = passport.authenticate('bearer', { session: false })
 
 // instantiate a router (mini app that only handles routes)
@@ -54,8 +47,6 @@ router.post('/sign-up', (req, res, next) => {
 		})
 		// create user with provided email and hashed password
 		.then((user) => User.create(user))
-		// send the new user object back with status 201, but `hashedPassword`
-		// won't be send because of the `transform` in the User model
 		.then((user) => res.status(201).json({ user: user.toObject() }))
 		// pass any errors along to the error handler
 		.catch(next)
@@ -66,18 +57,13 @@ router.post('/sign-up', (req, res, next) => {
 router.post('/sign-in', (req, res, next) => {
 	const pw = req.body.credentials.password
 	let user
-
-	// find a user based on the email that was passed
 	User.findOne({ email: req.body.credentials.email })
 		.then((record) => {
 			// if we didn't find a user with that email, send 401
 			if (!record) {
 				throw new BadCredentialsError()
 			}
-			// save the found user outside the promise chain
 			user = record
-			// `bcrypt.compare` will return true if the result of hashing `pw`
-			// is exactly equal to the hashed password stored in the DB
 			return bcrypt.compare(pw, user.hashedPassword)
 		})
 		.then((correctPassword) => {
@@ -102,27 +88,21 @@ router.post('/sign-in', (req, res, next) => {
 })
 
 // CHANGE password
-// PATCH /change-password
+// PATCH 
 router.patch('/change-password', requireToken, (req, res, next) => {
 	let user
 	// `req.user` will be determined by decoding the token payload
 	User.findById(req.user.id)
-		// save user outside the promise chain
 		.then((record) => {
 			user = record
 		})
 		// check that the old password is correct
 		.then(() => bcrypt.compare(req.body.passwords.old, user.hashedPassword))
-		// `correctPassword` will be true if hashing the old password ends up the
-		// same as `user.hashedPassword`
 		.then((correctPassword) => {
-			// throw an error if the new password is missing, an empty string,
-			// or the old password was wrong
 			if (!req.body.passwords.new || !correctPassword) {
 				throw new BadParamsError()
 			}
 		})
-		// hash the new password
 		.then(() => bcrypt.hash(req.body.passwords.new, bcryptSaltRounds))
 		.then((hash) => {
 			// set and save the new hashed password in the DB
